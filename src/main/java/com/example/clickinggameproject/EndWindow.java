392da4934efb538101c6aced34f1404c4b2001e0
Adrien KIAT <adrien.kiat15@gmail.com>
2022-05-25 18:53:05 -0400

    Total clicks

--- src/main/java/com/example/clickinggameproject/EndWindow.java
+++ src/main/java/com/example/clickinggameproject/EndWindow.java
@@ -2,9 +2,10 @@ package com.example.clickinggameproject;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+
 
 public class EndWindow {
 
@@ -13,32 +14,32 @@ public class EndWindow {
 
         Stage window = new Stage();
 
-        HBox root = new HBox();
+        VBox root = new VBox();
 
         window.initModality(Modality.APPLICATION_MODAL);
 
-        window.setTitle("SharpShooter");
-
+        window.setTitle("Shxpes-Clixker");
 
         //Number of clicks on the circle button
         Label circle = new Label();
 
-        circle.setText("Total cicks on circle button: 0");
+        circle.setText("Total clicks on circle the button: ");
 
         //Number of clicks on the square button
         Label square = new Label();
 
-        square.setText("Total clicks on square button: 0");
+        square.setText("Total clicks on square the button: ");
 
-        root.getChildren().addAll(circle,square);
+        root.getChildren().addAll(circle, square);
 
         root.setSpacing(30);
 
-        Scene scene = new Scene(root, 1000,500);
+        Scene scene = new Scene(root, 1000, 500);
 
         window.setScene(scene);
 
         window.showAndWait();
 
     }
+
 }
