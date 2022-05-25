ddca75a4f50b66116e14397786b4c77103916164
Adrien KIAT <adrien.kiat15@gmail.com>
2022-05-24 20:12:04 -0400

    Initial commit

---
+++ src/main/java/com/example/clickinggameproject/HelloApplication.java
@@ -0,0 +1,61 @@
+package com.example.clickinggameproject;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Pos;
+
+
+public class HelloApplication extends Application {
+
+    Label label;
+    Stage window;
+    Button button;
+
+    public static void main(String[] args) {
+        //Launch the application
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        //Set stage title
+        primaryStage.setTitle("My reaction game");
+
+        //Create Label and Button
+        Label messageLabel = new Label("SharpShooter");
+
+        label = new Label("Click to start the game. ");
+
+        button = new Button("Click here");
+
+        //Action happening when a button is clicked(Open a new window)
+        button.setOnAction(e -> NewWindow.display());
+
+        //Create VBox
+        StackPane layout = new StackPane();
+
+        layout.getChildren().add(button);
+
+        VBox vbox = new VBox(messageLabel,label, button, layout);
+
+        vbox.setAlignment(Pos.CENTER);
+
+        //Set Margins
+        vbox.setMargin(label, new Insets(100,50 , 20, 50));
+
+        Scene scene =  new Scene(vbox, 500, 500);
+
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+
+    }
+
+}
---
+++ src/main/java/com/example/clickinggameproject/NewWindow.java
@@ -0,0 +1,105 @@
+package com.example.clickinggameproject;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+public class NewWindow {
+
+
+    private static double r = 50;
+
+    static int count1 = 0;
+    static int count2 = 0;
+
+    public static void display() {
+
+        Stage window = new Stage();
+
+        HBox root = new HBox();
+
+
+
+        window.initModality(Modality.APPLICATION_MODAL);
+
+        window.setTitle("Sharpshooter");
+
+        Label circle = new Label();
+
+        circle.setText("Clicks on circle: 0");
+
+        circle.setMaxHeight(25);
+
+        circle.setMaxWidth(25);
+
+        circle.setScaleX(25);
+
+        circle.setScaleY(25);
+
+        Label square = new Label();
+
+        square.setText("Clicks on square: 0");
+
+        square.setMaxHeight(25);
+
+        square.setMaxWidth(25);
+
+        square.setScaleX(25);
+
+        square.setScaleY(25);
+
+
+        //Round Button
+        Button roundbutton = new Button("Circle");
+
+        roundbutton.setShape(new Circle(r));
+
+        roundbutton.setMinSize(2*r, 2*r);
+
+        roundbutton.setMaxSize(2*r, 2*r);
+
+
+
+        roundbutton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                count1++;
+                circle.setText(Integer.toString(count1));
+            }
+        });
+
+        //Square Button
+        Button squarebutton = new Button("Square");
+
+        squarebutton.setMinSize(100,100);
+
+        squarebutton.setMaxSize(100,100);
+
+        squarebutton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                count2++;
+                square.setText(Integer.toString(count2));
+            }
+        });
+
+
+        root.getChildren().addAll(circle, square,roundbutton,squarebutton);
+
+        root.setAlignment(Pos.CENTER);
+        root.setSpacing(30);
+
+        Scene scene = new Scene(root, 500,500);
+
+        window.setScene(scene);
+
+        window.showAndWait();
+    }
+}
